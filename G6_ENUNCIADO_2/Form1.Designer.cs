3caef19a56d8f04c44688564e8587e14cca7207c
paribrayam <N00365419@upn.pe>
2025-04-27 17:36:47 -0500

    cambio en mi codigo de visual

--- G6_ENUNCIADO_2/Form1.Designer.cs
+++ G6_ENUNCIADO_2/Form1.Designer.cs
@@ -55,23 +55,28 @@ namespace G6_ENUNCIADO_2
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Números});
-            this.dataGridView1.Location = new System.Drawing.Point(533, 29);
+            this.dataGridView1.Location = new System.Drawing.Point(711, 36);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(146, 267);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(195, 329);
             this.dataGridView1.TabIndex = 0;
             // 
             // Números
             // 
             this.Números.HeaderText = "Números";
+            this.Números.MinimumWidth = 6;
             this.Números.Name = "Números";
+            this.Números.Width = 125;
             // 
             // G6_BtnInsertar
             // 
             this.G6_BtnInsertar.BackColor = System.Drawing.Color.Gold;
             this.G6_BtnInsertar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.G6_BtnInsertar.Location = new System.Drawing.Point(28, 65);
+            this.G6_BtnInsertar.Location = new System.Drawing.Point(37, 80);
+            this.G6_BtnInsertar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.G6_BtnInsertar.Name = "G6_BtnInsertar";
-            this.G6_BtnInsertar.Size = new System.Drawing.Size(207, 23);
+            this.G6_BtnInsertar.Size = new System.Drawing.Size(276, 28);
             this.G6_BtnInsertar.TabIndex = 1;
             this.G6_BtnInsertar.Text = "Numeros ingresados de la lista :";
             this.G6_BtnInsertar.UseVisualStyleBackColor = false;
@@ -79,18 +84,20 @@ namespace G6_ENUNCIADO_2
             // 
             // textMostrar
             // 
-            this.textMostrar.Location = new System.Drawing.Point(253, 69);
+            this.textMostrar.Location = new System.Drawing.Point(337, 85);
+            this.textMostrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textMostrar.Name = "textMostrar";
-            this.textMostrar.Size = new System.Drawing.Size(245, 20);
+            this.textMostrar.Size = new System.Drawing.Size(325, 22);
             this.textMostrar.TabIndex = 2;
             // 
             // G6_BtnAscendente
             // 
             this.G6_BtnAscendente.BackColor = System.Drawing.Color.Gold;
             this.G6_BtnAscendente.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.G6_BtnAscendente.Location = new System.Drawing.Point(28, 119);
+            this.G6_BtnAscendente.Location = new System.Drawing.Point(37, 146);
+            this.G6_BtnAscendente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.G6_BtnAscendente.Name = "G6_BtnAscendente";
-            this.G6_BtnAscendente.Size = new System.Drawing.Size(207, 24);
+            this.G6_BtnAscendente.Size = new System.Drawing.Size(276, 30);
             this.G6_BtnAscendente.TabIndex = 3;
             this.G6_BtnAscendente.Text = "Ordenar de forma ascendente :";
             this.G6_BtnAscendente.UseVisualStyleBackColor = false;
@@ -98,18 +105,20 @@ namespace G6_ENUNCIADO_2
             // 
             // ResulAscen
             // 
-            this.ResulAscen.Location = new System.Drawing.Point(253, 122);
+            this.ResulAscen.Location = new System.Drawing.Point(337, 150);
+            this.ResulAscen.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ResulAscen.Name = "ResulAscen";
-            this.ResulAscen.Size = new System.Drawing.Size(245, 20);
+            this.ResulAscen.Size = new System.Drawing.Size(325, 22);
             this.ResulAscen.TabIndex = 4;
             // 
             // G6_BtnDescendente
             // 
             this.G6_BtnDescendente.BackColor = System.Drawing.Color.Gold;
             this.G6_BtnDescendente.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.G6_BtnDescendente.Location = new System.Drawing.Point(28, 172);
+            this.G6_BtnDescendente.Location = new System.Drawing.Point(37, 212);
+            this.G6_BtnDescendente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.G6_BtnDescendente.Name = "G6_BtnDescendente";
-            this.G6_BtnDescendente.Size = new System.Drawing.Size(207, 26);
+            this.G6_BtnDescendente.Size = new System.Drawing.Size(276, 32);
             this.G6_BtnDescendente.TabIndex = 5;
             this.G6_BtnDescendente.Text = "Ordenar de forma descendente :";
             this.G6_BtnDescendente.UseVisualStyleBackColor = false;
@@ -117,18 +126,20 @@ namespace G6_ENUNCIADO_2
             // 
             // txtResulDesn
             // 
-            this.txtResulDesn.Location = new System.Drawing.Point(253, 176);
+            this.txtResulDesn.Location = new System.Drawing.Point(337, 217);
+            this.txtResulDesn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtResulDesn.Name = "txtResulDesn";
-            this.txtResulDesn.Size = new System.Drawing.Size(245, 20);
+            this.txtResulDesn.Size = new System.Drawing.Size(325, 22);
             this.txtResulDesn.TabIndex = 6;
             // 
             // G6_BtnBuscar
             // 
             this.G6_BtnBuscar.BackColor = System.Drawing.Color.Gold;
             this.G6_BtnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.G6_BtnBuscar.Location = new System.Drawing.Point(28, 225);
+            this.G6_BtnBuscar.Location = new System.Drawing.Point(37, 277);
+            this.G6_BtnBuscar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.G6_BtnBuscar.Name = "G6_BtnBuscar";
-            this.G6_BtnBuscar.Size = new System.Drawing.Size(207, 24);
+            this.G6_BtnBuscar.Size = new System.Drawing.Size(276, 30);
             this.G6_BtnBuscar.TabIndex = 7;
             this.G6_BtnBuscar.Text = "Buscar un elemento de la lista :";
             this.G6_BtnBuscar.UseVisualStyleBackColor = false;
@@ -136,27 +147,30 @@ namespace G6_ENUNCIADO_2
             // 
             // txtNumero
             // 
-            this.txtNumero.Location = new System.Drawing.Point(253, 229);
+            this.txtNumero.Location = new System.Drawing.Point(337, 282);
+            this.txtNumero.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNumero.Name = "txtNumero";
-            this.txtNumero.Size = new System.Drawing.Size(245, 20);
+            this.txtNumero.Size = new System.Drawing.Size(325, 22);
             this.txtNumero.TabIndex = 8;
             // 
             // lblNum
             // 
             this.lblNum.AutoSize = true;
             this.lblNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNum.Location = new System.Drawing.Point(271, 337);
+            this.lblNum.Location = new System.Drawing.Point(361, 415);
+            this.lblNum.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNum.Name = "lblNum";
-            this.lblNum.Size = new System.Drawing.Size(0, 15);
+            this.lblNum.Size = new System.Drawing.Size(0, 18);
             this.lblNum.TabIndex = 9;
             this.lblNum.Click += new System.EventHandler(this.label1_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::G6_ENUNCIADO_2.Properties.Resources.logo_4;
-            this.pictureBox1.Location = new System.Drawing.Point(27, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(36, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(99, 41);
+            this.pictureBox1.Size = new System.Drawing.Size(132, 50);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 10;
             this.pictureBox1.TabStop = false;
@@ -164,9 +178,10 @@ namespace G6_ENUNCIADO_2
             // btnLimpiar
             // 
             this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLimpiar.Location = new System.Drawing.Point(59, 321);
+            this.btnLimpiar.Location = new System.Drawing.Point(79, 395);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnLimpiar.Name = "btnLimpiar";
-            this.btnLimpiar.Size = new System.Drawing.Size(98, 44);
+            this.btnLimpiar.Size = new System.Drawing.Size(131, 54);
             this.btnLimpiar.TabIndex = 11;
             this.btnLimpiar.Text = "Ingresar nuevos datos";
             this.btnLimpiar.UseVisualStyleBackColor = true;
@@ -176,9 +191,10 @@ namespace G6_ENUNCIADO_2
             // 
             this.btnEliminar.BackColor = System.Drawing.Color.Gold;
             this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEliminar.Location = new System.Drawing.Point(28, 274);
+            this.btnEliminar.Location = new System.Drawing.Point(37, 337);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(207, 23);
+            this.btnEliminar.Size = new System.Drawing.Size(276, 28);
             this.btnEliminar.TabIndex = 12;
             this.btnEliminar.Text = "Eliminar un número :";
             this.btnEliminar.UseVisualStyleBackColor = false;
@@ -186,17 +202,19 @@ namespace G6_ENUNCIADO_2
             // 
             // txtEliminar
             // 
-            this.txtEliminar.Location = new System.Drawing.Point(253, 276);
+            this.txtEliminar.Location = new System.Drawing.Point(337, 340);
+            this.txtEliminar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtEliminar.Name = "txtEliminar";
-            this.txtEliminar.Size = new System.Drawing.Size(245, 20);
+            this.txtEliminar.Size = new System.Drawing.Size(325, 22);
             this.txtEliminar.TabIndex = 13;
             // 
             // btnCerrar
             // 
             this.btnCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCerrar.Location = new System.Drawing.Point(562, 321);
+            this.btnCerrar.Location = new System.Drawing.Point(749, 395);
+            this.btnCerrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCerrar.Name = "btnCerrar";
-            this.btnCerrar.Size = new System.Drawing.Size(88, 44);
+            this.btnCerrar.Size = new System.Drawing.Size(117, 54);
             this.btnCerrar.TabIndex = 14;
             this.btnCerrar.Text = "Cerrar";
             this.btnCerrar.UseVisualStyleBackColor = true;
@@ -204,9 +222,10 @@ namespace G6_ENUNCIADO_2
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(253, 329);
+            this.button1.Location = new System.Drawing.Point(337, 405);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(134, 23);
+            this.button1.Size = new System.Drawing.Size(179, 28);
             this.button1.TabIndex = 15;
             this.button1.Text = "Bienvenido usuario :)";
             this.button1.UseVisualStyleBackColor = true;
@@ -214,9 +233,10 @@ namespace G6_ENUNCIADO_2
             // 
             // Lista
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(707, 392);
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(943, 482);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnCerrar);
             this.Controls.Add(this.txtEliminar);
@@ -233,6 +253,7 @@ namespace G6_ENUNCIADO_2
             this.Controls.Add(this.textMostrar);
             this.Controls.Add(this.G6_BtnInsertar);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Lista";
             this.Text = "Lista";
             this.Load += new System.EventHandler(this.Lista_Load);
