21d65cd77be9af85a3774ab4a702dffb29e2061a
Angelina-Casas <n00387993@upn.pe>
2025-04-27 16:58:27 -0500

    Agregando un label a mi codigo de visual studio en la rama-2

--- G6_ENUNCIADO_2/Form1.Designer.cs
+++ G6_ENUNCIADO_2/Form1.Designer.cs
@@ -46,6 +46,7 @@ namespace G6_ENUNCIADO_2
             this.txtEliminar = new System.Windows.Forms.TextBox();
             this.btnCerrar = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -204,7 +205,7 @@ namespace G6_ENUNCIADO_2
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(253, 329);
+            this.button1.Location = new System.Drawing.Point(274, 334);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(134, 23);
             this.button1.TabIndex = 15;
@@ -212,11 +213,22 @@ namespace G6_ENUNCIADO_2
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label1.Location = new System.Drawing.Point(271, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Ibgresa los numero en las casillas de la lista";
+            // 
             // Lista
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(707, 392);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnCerrar);
             this.Controls.Add(this.txtEliminar);
@@ -262,6 +274,7 @@ namespace G6_ENUNCIADO_2
         private System.Windows.Forms.TextBox txtEliminar;
         private System.Windows.Forms.Button btnCerrar;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
